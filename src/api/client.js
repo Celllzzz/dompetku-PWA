310d334d894ae109ebfc157e11878f50f513def7
Celllzzz <maya19.0316@gmail.com>
2025-11-28 15:33:58 +0700

    fixing bug

--- src/api/client.js
+++ src/api/client.js
@@ -1,11 +1,29 @@
-// src/api/client.js
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // Ambil dari .env
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Mengambil https://api-dompetku.vercel.app/api
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+apiClient.interceptors.request.use((config) => {
+  const storedUser = localStorage.getItem("user_dompetku");
+  
+  if (storedUser) {
+    try {
+      const user = JSON.parse(storedUser);
+      // Jika user login, ambil ID-nya dan tempel ke header
+      if (user?.id) {
+        config.headers['user-id'] = user.id; 
+      }
+    } catch (error) {
+      console.error("Gagal membaca user data:", error);
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default apiClient;
